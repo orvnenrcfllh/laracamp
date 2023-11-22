fb55a32e7e726c8aab0a636102acf1ebf4f41213
Orvin Enrico Fallah <orvnenrcfllh@gmail.com>
2023-11-22 14:28:58 +0700

    update dashboard admin and user

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -34,10 +34,11 @@
                                     <strong>${{ $checkout->Camp->price }}K</strong>
                                 </td>
                                 <td>
-                                    @if ($checkout->is_paid)
-                                        <strong class="text-success">Payment Success</strong>
-                                    @else
-                                        <strong>Waiting for Payment</strong>
+                                    <strong>{{ $checkout->payment_status }}</strong>
+                                </td>
+                                <td>
+                                    @if ($checkout->payment_status == 'waiting')
+                                        <a href="{{ $checkout->midtrans_url }}" class="btn btn-primary">Pay Here</a>
                                     @endif
                                 </td>
                                 <td>
@@ -49,7 +50,7 @@
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="5">No Data</td>
+                                <td colspan="5">No Camp Registered</td>
                             </tr>
                         @endforelse
                     </tbody>
